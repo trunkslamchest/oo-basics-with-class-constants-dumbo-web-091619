d9abb499e93bc9f8432505b2570beeb4a1d48492
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:57:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  brands = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -10,7 +10,7 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    brands << "tets"
+    BRANDS << "tets"
   end
 
   # def BRANDS
